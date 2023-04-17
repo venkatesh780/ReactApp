ff06aef221eac03c6f7c3346f606c6bcb7c80ca9
venkatesh Thimmaiah <venkatesh.thimmaiahgari.20@mountblue.tech>
2023-04-17 14:54:45 +0530

    created head, body and footer and added cards to body

--- app.js
+++ app.js
@@ -1,17 +1,505 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "title1" }, "Hello World");
-const heading2 = React.createElement(
-  "h2",
-  { className: "tittle2" },
-  "Hello React"
+const Header = () => (
+  <div className="header">
+    <a href="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfLQFCS9itwlhghSfX0PzpzVCx4k-3aFuenNXu0fI&s"
+      />
+    </a>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
 );
 
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+const restroCards = [
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "3369",
+      name: "Truffles",
+      uuid: "992d9ef3-bef1-4d1a-8a96-3ced9e363d43",
+      city: "1",
+      area: "St Marks Road",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "cd832b6167eb9f88aeb1ccdebf38d942",
+      cuisines: ["American", "Desserts", "Continental", "Italian"],
+      tags: [],
+      costForTwo: 45000,
+      costForTwoString: "₹450 FOR TWO",
+      deliveryTime: 30,
+      minDeliveryTime: 30,
+      maxDeliveryTime: 30,
+      slaString: "30 MINS",
+      lastMileTravel: 0.8999999761581421,
+      slugs: {
+        restaurant: "truffles-ice-spice-central-bangalore",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address: "22, St. Marks Road, Bangalore",
+      locality: "Ashok Nagar",
+      parentId: 218065,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3000,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3000,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3000",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=6512447~p=1~eid=00000187-8e31-7094-1cb7-250f00b10154",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "0.8 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "3369",
+        deliveryTime: 30,
+        minDeliveryTime: 30,
+        maxDeliveryTime: 30,
+        lastMileTravel: 0.8999999761581421,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "4.5",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "3241",
+      name: "Meghana Foods",
+      uuid: "93695df0-56e1-417e-b9a8-32084ebaadaf",
+      city: "1",
+      area: "Ashok Nagar",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "e0vvulfbahjxjz6k4uwi",
+      cuisines: [
+        "Biryani",
+        "Andhra",
+        "South Indian",
+        "North Indian",
+        "Chinese",
+        "Seafood",
+      ],
+      tags: [],
+      costForTwo: 50000,
+      costForTwoString: "₹500 FOR TWO",
+      deliveryTime: 32,
+      minDeliveryTime: 32,
+      maxDeliveryTime: 32,
+      slaString: "32 MINS",
+      lastMileTravel: 2.4000000953674316,
+      slugs: {
+        restaurant: "meghana-foods-central-bangalore",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address:
+        "57/1, 1st Floor, Jayalaxmi Chambers, Next to Old Galaxy Theatre,Residency Road, Bangalore",
+      locality: "Residency Road",
+      parentId: 635,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3000,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3000,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3000",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "2.4 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "3241",
+        deliveryTime: 32,
+        minDeliveryTime: 32,
+        maxDeliveryTime: 32,
+        lastMileTravel: 2.4000000953674316,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "4.5",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "3434",
+      name: "Nandhini Deluxe",
+      uuid: "24ba68a5-9a46-4f74-8ed2-4536d3692b80",
+      city: "1",
+      area: "St Marks Road",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "dlt4ml0id5wei4yi840b",
+      cuisines: ["Andhra", "Biryani", "Chinese", "North Indian"],
+      tags: [],
+      costForTwo: 50000,
+      costForTwoString: "₹500 FOR TWO",
+      deliveryTime: 25,
+      minDeliveryTime: 25,
+      maxDeliveryTime: 25,
+      slaString: "25 MINS",
+      lastMileTravel: 0.8999999761581421,
+      slugs: {
+        restaurant: "hotel-nandhini-central-bangalore",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address:
+        " #22, 1st floor, St Marks Road, Shanthala nagar , Ashok Nagar, Bangalore -560001",
+      locality: "Ashok Nagar",
+      parentId: 2451,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "FLAT100 off",
+        shortDescriptionList: [
+          {
+            meta: "FLAT100 off | Use FLATDEAL",
+            discountType: "Flat",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "FLAT100 off | Use FLATDEAL",
+            discountType: "Flat",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "₹100 OFF",
+        shortDescriptionList: [
+          {
+            meta: "Use FLATDEAL",
+            discountType: "Flat",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "FLAT100 off | Use FLATDEAL",
+            discountType: "Flat",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3000,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3000,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3000",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=6509576~p=4~eid=00000187-8e31-7094-1cb7-251000b10422",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "0.8 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "3434",
+        deliveryTime: 25,
+        minDeliveryTime: 25,
+        maxDeliveryTime: 25,
+        lastMileTravel: 0.8999999761581421,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "4.2",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "121603",
+      name: "Kannur Food Point",
+      uuid: "51983905-e698-4e31-b0d7-e376eca56320",
+      city: "1",
+      area: "Tavarekere",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
+      cuisines: ["Kerala", "Chinese"],
+      tags: [],
+      costForTwo: 30000,
+      costForTwoString: "₹300 FOR TWO",
+      deliveryTime: 36,
+      minDeliveryTime: 36,
+      maxDeliveryTime: 36,
+      slaString: "36 MINS",
+      lastMileTravel: 6.699999809265137,
+      slugs: {
+        restaurant: "kannur-food-point-btm",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address:
+        "6/21,9TH CROSS ,1ST MAIN, VENKATESHWARA LAYOUT,SG PALYA, BENGALURU, - 560093",
+      locality: "SG Palya",
+      parentId: 20974,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 5400,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 5400,
+        message: "",
+        title: "Delivery Charge",
+        amount: "5400",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 1,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "6.6 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "121603",
+        deliveryTime: 36,
+        minDeliveryTime: 36,
+        maxDeliveryTime: 36,
+        lastMileTravel: 6.699999809265137,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "IT_IS_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "3.9",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+];
+const RestarentCard = (props) => {
+  console.log(typeof props);
+  return (
+    <div className="restarent-card">
+      <img
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" +
+          props.restarunt.cloudinaryImageId
+        }
+      />
+      <h3>{props.restarunt.name}</h3>
+      <h4>{props.restarunt.cuisines.join(", ")}</h4>
+      <h4>{props.restarunt.avgRating} Stars</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      {restroCards.map((restarunt) => {
+        return (
+          <RestarentCard restarunt={restarunt.data} key={restarunt.data.id} />
+        );
+      })}
+    </div>
+  );
+};
+
+const Footer = () => <h4>Footer</h4>;
+
+const AppLayout = () => (
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<AppLayout />);
